d028b6092dbdabdfd1c96d55b617710f8ce7da84
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-03-22 23:45:50 +0530

    Update update-tsc-json.js

--- .github/workflows/update-tsc-json.js
+++ .github/workflows/update-tsc-json.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const CODEOWNERS_PATH = process.argv[2];
-const TSC_JSON_PATH = path.join(process.cwd(), 'tsc.json');
+const TSC_JSON_PATH = path.join(process.env.TSC_JSON_PATH, 'tsc.json');
 
 const tscJson = require(TSC_JSON_PATH);
 
