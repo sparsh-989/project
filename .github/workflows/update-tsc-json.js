bb42e71753b3519d200b0e62b596174f6ce029d3
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-03-24 00:07:48 +0530

    Update update-tsc-json.js

--- .github/workflows/update-tsc-json.js
+++ .github/workflows/update-tsc-json.js
@@ -2,10 +2,8 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
-const MAIN_BRANCH_REF = "refs/heads/main";
-
 // Find the path to the modified CODEOWNERS file
-const modifiedFiles = execSync("git diff --name-only HEAD~1 HEAD", {
+const modifiedFiles = execSync("git diff-tree --no-commit-id --name-only -r HEAD", {
   encoding: "utf8",
 });
 const modifiedCodeowners = modifiedFiles
@@ -17,28 +15,28 @@ if (!modifiedCodeowners) {
   process.exit(1);
 }
 
-const CODEOWNERS_PATH = modifiedCodeowners;
+const CODEOWNERS_PATH = process.env.CODEOWNERS_PATH;
 console.log(`CODEOWNERS_PATH: ${CODEOWNERS_PATH}`);
 
-// Checkout the main branch and get the path to tsc.json
-execSync(`git checkout ${MAIN_BRANCH_REF}`);
-const TSC_JSON_PATH = path.join(process.cwd(), "tsc.json");
-
-// Switch back to the branch where changes were made
-execSync(`git checkout -`);
+// Read the codeowners file diff between the latest commit and the previous commit
+const codeownersDiff = execSync(`git diff HEAD~1 HEAD -- ${CODEOWNERS_PATH}`, {
+  encoding: "utf8",
+});
+console.log('codeownersDiff:', codeownersDiff);
 
 // Read the content of the CODEOWNERS file
 const codeowners = fs.readFileSync(CODEOWNERS_PATH, "utf8");
 console.log('CODEOWNERS file:', codeowners);
 
-const regex = /^(\w+)(?:\/\w+)*\s+@(\w+)/gm;
+const regex = /^\+\s*(\w+)(?:\/\w+)*\s+@(\w+)/gm;
 let match;
 
 // Read the tsc.json file
+const TSC_JSON_PATH = path.join(process.cwd(), "tsc.json");
 const tscJson = JSON.parse(fs.readFileSync(TSC_JSON_PATH, "utf8"));
 
-// Iterate through the lines in the codeowners file and update tsc.json
-while ((match = regex.exec(codeowners)) !== null) {
+// Iterate through the added lines in the codeowners file and update tsc.json
+while ((match = regex.exec(codeownersDiff)) !== null) {
   console.log('Match found:', match);
   const repoName = match[1];
   const githubUsername = match[2];
@@ -60,4 +58,4 @@ while ((match = regex.exec(codeowners)) !== null) {
   }
 }
 
-fs.writeFileSync(TSC_JSON_PATH, JSON.stringify(tscJson, null, 2));
+fs.writeFileSync(TSC_JSON_PATH, JSON.stringify(tscJson
