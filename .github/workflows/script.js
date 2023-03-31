5a904449303ca62f1d9b8f07846afab7208ef5f4
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-03-31 15:20:16 +0530

    Create script.js

---
+++ .github/workflows/script.js
@@ -0,0 +1,64 @@
+const { Octokit } = require("@octokit/core");
+
+const octokit = new Octokit({
+  auth: "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",
+});
+const owner = "Saurabh-Mokashi"; // Replace with the repository owner's username
+const repo = "push_swap"; // Replace with the repository name
+const fullCommitId = process.argv[2]; // Get the full commit ID from the command line argument
+const commitId = fullCommitId.slice(0, 7); // Extract the first 7 characters of the commit ID
+
+
+
+async function getUsernameByCommitId() {
+  try {
+    const response = await octokit.request("GET /repos/{owner}/{repo}/commits/{ref}", {
+      owner,
+      repo,
+      ref: commitId,
+    });
+
+    if (response.data && response.data.author) {
+      const username = response.data.author.login;
+      console.log(`Author's username: ${username}`);
+      getUserTypeByUsername(username);
+    } else {
+      console.log("Commit or author not found.");
+    }
+  } catch (error) {
+    console.error("Error fetching commit data:", error.message);
+  }
+}
+
+async function getUserTypeByUsername(username) {
+  if (username === "actions-user") {
+    console.log("The user is a bot.");
+  } else {
+    try {
+      const response = await octokit.request("GET /users/{username}", {
+        username,
+      });
+
+      if (response.data) {
+        const userType = response.data.type;
+        console.log(`User type: ${userType}`);
+        if (userType === "Bot") {
+          console.log("The user is a bot.");
+        } else {
+          console.log("The user is a human.");
+        }
+      } else {
+        console.log("User not found.");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error.message);
+    }
+  }
+}
+
+getUsernameByCommitId();
+
+  
+
+
+
