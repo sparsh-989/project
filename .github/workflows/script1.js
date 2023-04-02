4993334d4d807b47b988d0e4d8fb8e7186a230c7
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-02 20:44:00 +0530

    Update script1.js

--- .github/workflows/script1.js
+++ .github/workflows/script1.js
@@ -1,62 +1,39 @@
 const { exec } = require('child_process');
-const fs = require('fs');
 
 (async () => {
   try {
     const commitId = process.argv[2];
+    const allowedChangesByHuman = ['linkedin', 'slack', 'twitter', 'availableForHire'];
 
-    // Fetch the necessary Git history
-    exec(`git fetch --depth=2`, (fetchError) => {
-      if (fetchError) {
-        console.error(`Fetch Error: ${fetchError.message}`);
+    exec(`git diff ${commitId}^ ${commitId} -- tsc.json`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
         return;
       }
 
-      // Checkout the previous version of tsc.json
-      exec(`git checkout ${commitId}^ -- tsc.json`, (error) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
+      if (stderr) {
+        console.error(`Error: ${stderr}`);
+        return;
+      }
+
+      const diffLines = stdout.split('\n');
+      let allowedChanges = true;
+
+      for (const line of diffLines) {
+        if (line.startsWith('+') || line.startsWith('-')) {
+          const match = line.match(/"([^"]+)":/);
+          if (match && match[1] && !allowedChangesByHuman.includes(match[1])) {
+            allowedChanges = false;
+            break;
+          }
         }
+      }
 
-        // Read the old tsc.json file
-        const oldTscJson = JSON.parse(fs.readFileSync('tsc.json'));
-
-        // Checkout the latest version of tsc.json
-        exec(`git checkout ${commitId} -- tsc.json`, (error) => {
-          if (error) {
-            console.error(`Error: ${error.message}`);
-            return;
-          }
-
-          // Read the new tsc.json file
-          const newTscJson = JSON.parse(fs.readFileSync('tsc.json'));
-
-          const allowedChangesByHuman = ['linkedin', 'slack', 'twitter', 'availableForHire'];
-
-          let allowedChanges = true;
-
-          outerLoop: for (let i = 0; i < Math.max(oldTscJson.length, newTscJson.length); i++) {
-            const oldEntry = oldTscJson[i] || {};
-            const newEntry = newTscJson[i] || {};
-
-            for (const key in { ...oldEntry, ...newEntry }) {
-              const isAllowedChange = allowedChangesByHuman.includes(key);
-
-              if (oldEntry[key] !== newEntry[key] && !isAllowedChange) {
-                allowedChanges = false;
-                break outerLoop;
-              }
-            }
-          }
-
-          if (allowedChanges) {
-            console.log('Changes made by a human are allowed.');
-          } else {
-            console.log('Changes should be made by a bot.');
-          }
-        });
-      });
+      if (allowedChanges) {
+        console.log('Changes made by a human are allowed.');
+      } else {
+        console.log('Changes should be made by a bot.');
+      }
     });
   } catch (error) {
     console.error('Error:', error.message);
