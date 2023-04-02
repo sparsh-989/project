5ffdf4fed108f5ab3df73e18cf6faaa98c591645
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-02 13:57:24 +0530

    Update getPRChanges.js

--- .github/workflows/getPRChanges/getPRChanges.js
+++ .github/workflows/getPRChanges/getPRChanges.js
@@ -3,7 +3,7 @@ const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({
     auth: "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",
-  });
+});
 
 async function getPullRequestChanges() {
   try {
@@ -27,7 +27,16 @@ async function getPullRequestChanges() {
 
 getPullRequestChanges().then((data) => {
   console.log(JSON.stringify(data));
+  if (!data) {
+    console.error("No data returned from getPullRequestChanges()");
+    return;
+  }
+  if (!data.files || data.files.length === 0) {
+    console.warn("No files were changed in the pull request.");
+    return;
+  }
   fs.writeFileSync('pr_changes.json', JSON.stringify(data.files));
   fs.writeFileSync('pull_request_number.txt', data.pullRequestNumber.toString());
+}).catch((error) => {
+  console.error("Error running getPullRequestChanges():", error);
 });
-
