aad95622151331377d2ee191bb1a03df3423132f
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-02 21:00:40 +0530

    Update script.js

--- .github/workflows/script.js
+++ .github/workflows/script.js
@@ -1,5 +1,4 @@
 const { Octokit } = require("@octokit/core");
-
 const octokit = new Octokit({
   auth: "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",
 });
@@ -7,8 +6,6 @@ const owner = "Sparsh-989"; // Replace with the repository owner's username
 const repo = "project"; // Replace with the repository name
 const fullCommitId = process.argv[2]; // Get the full commit ID from the command line argument
 const commitId = fullCommitId.slice(0, 7); // Extract the first 7 characters of the commit ID
-
-
 
 async function getUsernameByCommitId() {
   try {
@@ -33,19 +30,21 @@ async function getUsernameByCommitId() {
 async function getUserTypeByUsername(username) {
   if (username === "actions-user") {
     console.log("The user is a bot.");
+    console.log("::set-output name=user_type::bot");
   } else {
     try {
       const response = await octokit.request("GET /users/{username}", {
         username,
       });
-
       if (response.data) {
         const userType = response.data.type;
         console.log(`User type: ${userType}`);
         if (userType === "Bot") {
           console.log("The user is a bot.");
+          console.log("::set-output name=user_type::bot");
         } else {
           console.log("The user is a human.");
+          console.log("::set-output name=user_type::human");
         }
       } else {
         console.log("User not found.");
