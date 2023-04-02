de2cdf6d60745b9cf08c94ab9009703742b95b99
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-02 13:57:37 +0530

    Update getPRChanges.js

--- .github/workflows/getPRChanges/getPRChanges.js
+++ .github/workflows/getPRChanges/getPRChanges.js
@@ -3,7 +3,7 @@ const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({
     auth: "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",
-  });
+});
 
 async function getPullRequestChanges() {
   try {
@@ -12,11 +12,10 @@ async function getPullRequestChanges() {
       repo: "project",
       pull_number: process.env.PULL_REQUEST_NUMBER,
     });
-       if (!response.data) {
+     if (!response.data) {
       console.error("Error fetching pull request changes: Response data is undefined");
       return;
     }
-
     return {
       pullRequestNumber: process.env.PULL_REQUEST_NUMBER,
       files: response.data,
@@ -28,7 +27,16 @@ async function getPullRequestChanges() {
 
 getPullRequestChanges().then((data) => {
   console.log(JSON.stringify(data));
+  if (!data) {
+    console.error("No data returned from getPullRequestChanges()");
+    return;
+  }
+  if (!data.files || data.files.length === 0) {
+    console.warn("No files were changed in the pull request.");
+    return;
+  }
   fs.writeFileSync('pr_changes.json', JSON.stringify(data.files));
   fs.writeFileSync('pull_request_number.txt', data.pullRequestNumber.toString());
+}).catch((error) => {
+  console.error("Error running getPullRequestChanges():", error);
 });
-
