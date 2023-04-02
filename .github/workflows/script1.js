dba5deeef034206163fe550f39f3fb11c7c02303
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-02 20:15:34 +0530

    Update script1.js

--- .github/workflows/script1.js
+++ .github/workflows/script1.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+const fs = require('fs');
 
 (async () => {
   try {
@@ -11,20 +12,45 @@ const { exec } = require('child_process');
         return;
       }
 
-      // Run git diff to show changes in tsc.json
-      exec(`git diff ${commitId}^ ${commitId} -- tsc.json`, (error, stdout, stderr) => {
+      // Checkout the previous version of tsc.json
+      exec(`git checkout ${commitId}^ -- tsc.json`, (error) => {
         if (error) {
           console.error(`Error: ${error.message}`);
           return;
         }
 
-        if (stderr) {
-          console.error(`Error: ${stderr}`);
-          return;
-        }
+        // Read the old tsc.json file
+        const oldTscJson = JSON.parse(fs.readFileSync('tsc.json'));
 
-        console.log('Changes in tsc.json:');
-        console.log(stdout);
+        // Checkout the latest version of tsc.json
+        exec(`git checkout ${commitId} -- tsc.json`, (error) => {
+          if (error) {
+            console.error(`Error: ${error.message}`);
+            return;
+          }
+
+          // Read the new tsc.json file
+          const newTscJson = JSON.parse(fs.readFileSync('tsc.json'));
+
+          const allowedChangesByHuman = ['linkedin', 'slack', 'twitter', 'availableForHire'];
+
+          let allowedChanges = true;
+
+          for (let i = 0; i < oldTscJson.length; i++) {
+            for (const key in oldTscJson[i]) {
+              if (oldTscJson[i][key] !== newTscJson[i][key] && !allowedChangesByHuman.includes(key)) {
+                allowedChanges = false;
+                break;
+              }
+            }
+          }
+
+          if (allowedChanges) {
+            console.log('Changes made by a human are allowed.');
+          } else {
+            console.log('Changes should be made by a bot.');
+          }
+        });
       });
     });
   } catch (error) {
@@ -32,3 +58,4 @@ const { exec } = require('child_process');
     process.exit(1);
   }
 })();
+
