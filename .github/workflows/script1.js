067869d81831f355a186c376fdba0fa9921c8893
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-03 13:49:08 +0530

    Update script1.js

--- .github/workflows/script1.js
+++ .github/workflows/script1.js
@@ -6,12 +6,10 @@ const fs = require("fs");
     const commitId = process.argv[2];
     const userType = process.argv[3];
 
-    // Read the old tsc.json file
-    const oldTscJson = JSON.parse(fs.readFileSync("tsc.json"));
-
-    // Allowed changes for different user types
     const allowedChangesByHuman = ["linkedin", "slack", "twitter", "availableForHire"];
-    const allowedChangesByBot = ["name", "repo", "githubid"];
+    const disallowedChangesByHuman = ["name", "github", "repos"];
+    const allowedChangesByBot = ["name", "repos", "github"];
+    const disallowedChangesByBot = ["linkedin", "slack", "twitter", "availableForHire"];
 
     // Checkout the latest version of tsc.json
     exec(`git checkout ${commitId} -- tsc.json`, async (error) => {
@@ -35,14 +33,20 @@ const fs = require("fs");
         let allowedChanges = true;
         const lines = gitDiffOutput.split('\n');
 
-        for (const line of lines) {
-          if (line.startsWith('+') && line.includes(':')) {
-            const key = line.split(':')[0].trim().slice(2, -1);
+        const modifiedKeys = lines
+          .filter(line => line.startsWith('+') && line.includes(':'))
+          .map(line => line.match(/^\+?"?(\w+)"?:/)[1]);
 
-            if (userType === "human" && !allowedChangesByHuman.includes(key)) {
+        if (userType === "human") {
+          for (const key of modifiedKeys) {
+            if (disallowedChangesByHuman.includes(key)) {
               allowedChanges = false;
               break;
-            } else if (userType === "bot" && !allowedChangesByBot.includes(key)) {
+            }
+          }
+        } else if (userType === "bot") {
+          for (const key of modifiedKeys) {
+            if (disallowedChangesByBot.includes(key)) {
               allowedChanges = false;
               break;
             }
