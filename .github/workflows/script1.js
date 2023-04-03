0dc61e5cff839c5191a955cf320d14fcfe5f7547
Sparsh Agrawal <61935139+sparsh-989@users.noreply.github.com>
2023-04-03 13:11:30 +0530

    Update script1.js

--- .github/workflows/script1.js
+++ .github/workflows/script1.js
@@ -1,22 +1,6 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 
-function allowedChangesCheck(oldObj, newObj, allowedChangesByHuman) {
-  const keys = new Set([...Object.keys(oldObj), ...Object.keys(newObj)]);
-
-  for (const key of keys) {
-    const oldValue = oldObj[key];
-    const newValue = newObj[key];
-
-    if (oldValue !== newValue) {
-      if (!allowedChangesByHuman.includes(key)) {
-        return false;
-      }
-    }
-  }
-  return true;
-}
-
 (async () => {
   try {
     const commitId = process.argv[2];
@@ -25,8 +9,12 @@ function allowedChangesCheck(oldObj, newObj, allowedChangesByHuman) {
     // Read the old tsc.json file
     const oldTscJson = JSON.parse(fs.readFileSync("tsc.json"));
 
+    // Allowed changes for different user types
+    const allowedChangesByHuman = ["linkedin", "slack", "twitter", "availableForHire"];
+    const allowedChangesByBot = ["name", "repo", "githubid"];
+
     // Checkout the latest version of tsc.json
-    exec(`git checkout ${commitId} -- tsc.json`, (error) => {
+    exec(`git checkout ${commitId} -- tsc.json`, async (error) => {
       if (error) {
         console.error(`Error: ${error.message}`);
         return;
@@ -35,27 +23,39 @@ function allowedChangesCheck(oldObj, newObj, allowedChangesByHuman) {
       // Read the new tsc.json file
       const newTscJson = JSON.parse(fs.readFileSync("tsc.json"));
 
-      const allowedChangesByHuman = ["linkedin", "slack", "twitter", "availableForHire"];
-
-      let allowedChanges = true;
-
-      outerLoop: for (let i = 0; i < oldTscJson.length; i++) {
-        const oldObj = oldTscJson[i];
-        const newObj = newTscJson[i];
-
-        if (!allowedChangesCheck(oldObj, newObj, allowedChangesByHuman)) {
-          allowedChanges = false;
-          break;
+      // Get the git diff output for tsc.json
+      exec(`git diff ${commitId} HEAD -- tsc.json`, (error, stdout) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          return;
         }
-      }
 
-      if (allowedChanges && userType === "human") {
-        console.log("Valid changes.");
-      } else if (!allowedChanges && userType === "bot") {
-        console.log("Valid changes.");
-      } else {
-        console.log("Invalid changes.");
-      }
+        const gitDiffOutput = stdout;
+
+        let allowedChanges = true;
+
+        // Iterate through the diff output and check if the changes are allowed
+        const regex = /^[-+]\s+"([^"]+)":/gm;
+        let match;
+
+        while ((match = regex.exec(gitDiffOutput)) !== null) {
+          const key = match[1];
+
+          if (userType === "human" && !allowedChangesByHuman.includes(key)) {
+            allowedChanges = false;
+            break;
+          } else if (userType === "bot" && !allowedChangesByBot.includes(key)) {
+            allowedChanges = false;
+            break;
+          }
+        }
+
+        if (allowedChanges) {
+          console.log("Valid changes.");
+        } else {
+          console.log("Invalid changes.");
+        }
+      });
     });
   } catch (error) {
     console.error("Error:", error.message);
